8625c4e49fc69c832f94d5b25c71e72fd05c1dcd
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-12-08 10:26:56 -0300

    create app

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'el boton de abajo sirve para incrementar:',
+              'el boton de abajo sirve para incrementar test:',
             ),
             Text(
               '$_counter',
