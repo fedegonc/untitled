873f2c7ed35433ebfbc9dfe2196d7a9d399f3875
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-12-13 21:22:38 -0300

    add input text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,21 @@ import '../components/custom_slider1.dart';
 import '../components/custom_slider2.dart';
 import '../components/TextInputCounter.dart';
 
+// Widget reutilizable para los contenedores
+class ResponsiveContainer extends StatelessWidget {
+  final Widget child;
+
+  const ResponsiveContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho de la pantalla
+      child: child,
+    );
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,20 +31,11 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho de la pantalla
-                child: const TextInputCounter(),
-              ),
+              const ResponsiveContainer(child: TextInputCounter()),
               const SizedBox(height: 40),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho de la pantalla
-                child: const CustomSlider1(),
-              ),
+              const ResponsiveContainer(child: CustomSlider1()),
               const SizedBox(height: 40),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho de la pantalla
-                child: const CustomSlider2(),
-              ),
+              const ResponsiveContainer(child: CustomSlider2()),
             ],
           ),
         ),
